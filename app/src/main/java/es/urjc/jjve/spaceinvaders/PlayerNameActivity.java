008e25cf3a7d8cd1d1f943eccd50b2522925a8a2
OscarVGM <32273239+OscarVGM@users.noreply.github.com>
2018-11-16 11:52:37 +0100

    Cambiado el cuadro para introducir texto por TextEdit del activity y creado TreeMap en clase PlayerNameActivity. No se añade la puntuación, solo el nombre.

--- app/src/main/java/es/urjc/jjve/spaceinvaders/PlayerNameActivity.java
+++ app/src/main/java/es/urjc/jjve/spaceinvaders/PlayerNameActivity.java
@@ -2,16 +2,30 @@ package es.urjc.jjve.spaceinvaders;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.EditText;
+
+import java.util.Comparator;
+import java.util.TreeMap<int,String>; //Valor la clave porque el comparator ordena por clave
 
 public class PlayerNameActivity extends AppCompatActivity implements OnClickListener{
+
+    public TreeMap namePunt = new TreeMap(Comparator.<int>naturalOrder()); //TreeMap ordenado por clave
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nombre_jugador);
+
+        EditText nombre = findViewById(R.id.nombre);
+
+        String namePlayer = "";
+        namePlayer = String.valueOf(nombre.getText()); //Nombre introducido guardado en namePlayer
+
+        namePunt.put(0, namePlayer); //el 0 debería ser la puntuación
     }
 
     @Override
