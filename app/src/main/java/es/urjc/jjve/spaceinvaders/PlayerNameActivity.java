0c590e2d8e636b39f4d72e8d2876d0afa87c8e15
Yoshiki <yoshiki.r@gmail.com>
2018-12-09 02:21:06 +0100

    Warning supression

--- app/src/main/java/es/urjc/jjve/spaceinvaders/PlayerNameActivity.java
+++ app/src/main/java/es/urjc/jjve/spaceinvaders/PlayerNameActivity.java
@@ -2,38 +2,29 @@ package es.urjc.jjve.spaceinvaders;
 
 import android.content.Context;
 import android.content.Intent;
-import android.database.Cursor;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TextInputEditText;
-import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.textclassifier.TextLinks;
-import android.widget.ImageView;
 
 import java.io.ByteArrayOutputStream;
-
-import es.urjc.jjve.spaceinvaders.controllers.Score;
-import es.urjc.jjve.spaceinvaders.controllers.ScoreManager;
+import java.util.Objects;
 
 public class PlayerNameActivity extends AppCompatActivity implements OnClickListener {
 
     private int score;
-    private Bitmap image;
     private String nombre;
-    private boolean photo = false;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nombre_jugador);
-        score = getIntent().getExtras().getInt("score");
+        score = Objects.requireNonNull(getIntent().getExtras()).getInt("score");
 
         View aceptar = findViewById(R.id.buttonAccept);     //Se busca cual es el botón de aceptar
         aceptar.setOnClickListener(this);                   //Se le añade el OnClickListener para poder pulsarlo.
@@ -44,8 +35,8 @@ public class PlayerNameActivity extends AppCompatActivity implements OnClickList
         if (v.getId() == findViewById(R.id.buttonAccept).getId()) {
 
             //Seleccionamos la caja de texto y asignamos su valor a una variable
-            TextInputEditText aceptar = (TextInputEditText) findViewById(R.id.editText);
-            this.nombre = aceptar.getText().toString();
+            TextInputEditText aceptar = findViewById(R.id.editText);
+            this.nombre = Objects.requireNonNull(aceptar.getText()).toString();
 
 
             takePicture(); //Toma una foto para almacenarla en el fichero de highScores
@@ -67,23 +58,17 @@ public class PlayerNameActivity extends AppCompatActivity implements OnClickList
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == 1 && data != null) {
             Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            assert extras != null;
+            Bitmap image = (Bitmap) extras.get("data");
             //Uri uri = (Uri) extras.get("data");
-            this.image = imageBitmap;
 
-
-            photo = true;
 
             Intent i = new Intent(getApplicationContext(), HighScoreActivity.class);
             System.out.println("Holi");
             i.putExtra("score", score);
             i.putExtra("nombre",nombre);
-            i.putExtra("uri",getImageUri(this.getApplicationContext(),image));
+            i.putExtra("uri",getImageUri(this.getApplicationContext(), image));
             startActivity(i);
-
-            //ToDo Reescalar el bitmap segun la pantalla
-            //ToDo Guardar referencia de la imagen en el fichero de puntos -> galleryAddPic();
-            //ToDo Mostrar el bitmap en la pantalla de puntuaciones
         }
     }
 
